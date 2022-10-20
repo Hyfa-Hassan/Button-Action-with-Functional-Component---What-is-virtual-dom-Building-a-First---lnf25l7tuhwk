91a3c84f9472f31baea4db8a6ebcbc93387bcd36
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-20 21:15:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div id="main">
       // Do not alter the main div
-    <Button id="click" onClick={()=>para(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}>Click</Button>
+    <Button id="click" onClick={()=>para(`<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}`>Click</Button>
     </div>
   );
 }
