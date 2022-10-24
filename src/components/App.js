51d0a536fc3c81ecfb40f730ff3e6e6dd3104c65
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-24 17:23:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,20 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  function para(paragraphs){
-    console.log(paragraphs)
+  const [show, setShow] = useState(false)
+  const handleClick = () => {
+        // console.log('djgfdj')
+        setShow(true)
   }
   return (
     <div id="main">
-      // Do not alter the main div
-    <Button id="click" onClick={()=>para(`<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}`>Click</Button>
+       <button id="click" type="submit" onClick={()=>{handleClick()}}>Click</button>
+       {show === true ? (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+      ) : undefined }
     </div>
   );
 }
-
 
 export default App;
